f375933ef238217f246e0d2c3a7094b40cba03da
Srijani Ghosh <srijani.ghosh.in@gmail.com>
2023-02-27 19:32:31 +0530

    script added

---
+++ js/scripts.js
@@ -0,0 +1,146 @@
+
+window.addEventListener('DOMContentLoaded', event => {
+
+    const sidebarWrapper = document.getElementById('sidebar-wrapper');
+    let scrollToTopVisible = false;
+    // Closes the sidebar menu
+    const menuToggle = document.body.querySelector('.menu-toggle');
+    menuToggle.addEventListener('click', event => {
+        event.preventDefault();
+        sidebarWrapper.classList.toggle('active');
+        _toggleMenuIcon();
+        menuToggle.classList.toggle('active');
+    })
+    // Closes responsive menu when a scroll trigger link is clicked
+    var scrollTriggerList = [].slice.call(document.querySelectorAll('#sidebar-wrapper .js-scroll-trigger'));
+    scrollTriggerList.map(scrollTrigger => {
+        scrollTrigger.addEventListener('click', () => {
+            sidebarWrapper.classList.remove('active');
+            menuToggle.classList.remove('active');
+            _toggleMenuIcon();
+        })
+    });
+    function _toggleMenuIcon() {
+        const menuToggleBars = document.body.querySelector('.menu-toggle > .fa-bars');
+        const menuToggleTimes = document.body.querySelector('.menu-toggle > .fa-xmark');
+        if (menuToggleBars) {
+            menuToggleBars.classList.remove('fa-bars');
+            menuToggleBars.classList.add('fa-xmark');
+        }
+        if (menuToggleTimes) {
+            menuToggleTimes.classList.remove('fa-xmark');
+            menuToggleTimes.classList.add('fa-bars');
+        }
+    }
+    // Scroll to top button appear
+    document.addEventListener('scroll', () => {
+        const scrollToTop = document.body.querySelector('.scroll-to-top');
+        if (document.documentElement.scrollTop > 100) {
+            if (!scrollToTopVisible) {
+                fadeIn(scrollToTop);
+                scrollToTopVisible = true;
+            }
+        } else {
+            if (scrollToTopVisible) {
+                fadeOut(scrollToTop);
+                scrollToTopVisible = false;
+            }
+        }
+    })
+})
+function fadeOut(el) {
+    el.style.opacity = 1;
+    (function fade() {
+        if ((el.style.opacity -= .1) < 0) {
+            el.style.display = "none";
+        } else {
+            requestAnimationFrame(fade);
+        }
+    })();
+};
+function fadeIn(el, display) {
+    el.style.opacity = 0;
+    el.style.display = display || "block";
+    (function fade() {
+        var val = parseFloat(el.style.opacity);
+        if (!((val += .1) > 1)) {
+            el.style.opacity = val;
+            requestAnimationFrame(fade);
+        }
+    })();
+};
+
+
+
+
+//subject-details form actions
+var survey_options = document.getElementById('survey_options');
+var add_more_fields = document.getElementById('add_more_fields');
+var remove_fields = document.getElementById('remove_fields');
+//add new fields to subject details
+add_more_fields.onclick = function () {
+
+    var newField = document.createElement('br');
+
+    survey_options.appendChild(newField);
+
+    var newField = document.createElement('input');
+    newField.setAttribute('type', 'text');
+    newField.setAttribute('name', 'survey_options[]');
+    newField.setAttribute('class', 'survey_options');
+    newField.setAttribute('siz', 50);
+    newField.setAttribute('placeholder', 'Subject Name');
+    survey_options.appendChild(newField);
+
+    var newField = document.createElement('input');
+    newField.setAttribute('type', 'text');
+    newField.setAttribute('name', 'survey_options[]');
+    newField.setAttribute('class', 'survey_options');
+    newField.setAttribute('siz', 50);
+    newField.setAttribute('placeholder', 'Faculty');
+    survey_options.appendChild(newField);
+
+    var newField = document.createElement('input');
+    newField.setAttribute('type', 'text');
+    newField.setAttribute('name', 'survey_options[]');
+    newField.setAttribute('class', 'survey_options');
+    newField.setAttribute('siz', 50);
+    newField.setAttribute('placeholder', 'Subject Code');
+    survey_options.appendChild(newField);
+
+    var newField = document.createElement('input');
+    newField.setAttribute('type', 'text');
+    newField.setAttribute('name', 'survey_options[]');
+    newField.setAttribute('class', 'survey_options');
+    newField.setAttribute('siz', 50);
+    newField.setAttribute('placeholder', 'Classes/Week');
+    survey_options.appendChild(newField);
+}
+//remove field from subject details
+remove_fields.onclick = function () {
+    var input_tags = survey_options.getElementsByTagName('input');
+    if (input_tags.length > 4) {
+        for (i = 0; i < 4; i++) {
+            survey_options.removeChild(input_tags[(input_tags.length) - 1]);
+        }
+    }
+};
+
+
+
+
+//data entry form actions
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+    coll[i].addEventListener("click", function () {
+        this.classList.toggle("active");
+        var content = this.nextElementSibling;
+        if (content.style.display === "block") {
+            content.style.display = "none";
+        } else {
+            content.style.display = "block";
+        }
+    });
+}
